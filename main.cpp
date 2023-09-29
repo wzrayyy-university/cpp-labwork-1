7d93baa2fd2b41a3fb214317e101450bed3a247c
Артур Хачатуров <143233939+wzrayyy@users.noreply.github.com>
2023-09-29 11:53:42 +0300

    Replace std::string with c-style char* arrays.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,13 @@
 #include <iostream>
-#include <string>
-#include <string_view>
-#include <utility>
-#include <vector>
 #include <cstdint>
 #include <format>
 #include <cstring>
 #include <fstream>
-#include <limits>
 
 const char kDefaultDelimiter = '\n';
 
 struct Config {
-  std::string filename;
+  char *filename{};
   uint64_t lines = -1;
   bool tail = false;
   char delimiter = kDefaultDelimiter;
@@ -23,31 +18,36 @@ class ArgumentParser {
   Config &config;
   int argc;
   char **argv;
-  bool is_lines_set;
+  bool is_lines_set = false;
+  bool is_filename_set = false;
 
   void PrintHelpMessage(bool is_error) {
-    std::string message = std::format("Usage:\n"
-                                      "\t {} [OPTIONS] [FILE]\n\n"
-                                      "Options:\n"
-                                      "\t-l, --lines=N\t\tPrints N lines to stdout.\n"
-                                      "\t-t, --tail\t\tReverse printing direction.\n"
-                                      "\t-d --delimiter=CHAR\tSet delimiter character to CHAR.\n", argv[0]);
-    if (is_error)
-      std::cerr << message;
-    else
-      std::cout << message;
+    const char options_text[] = "Options:\n"
+                                "\t-l, --lines=N\t\tPrints N lines to stdout.\n"
+                                "\t-t, --tail\t\tReverse printing direction.\n"
+                                "\t-d --delimiter=CHAR\tSet delimiter character to CHAR.\n";
+
+    if (is_error) {
+      std::cerr << "\"Usage:\\n\"\n";
+      std::cerr << "\t " << argv[0] << R"( [OPTIONS] [FILE]\n\n")";
+      std::cerr << options_text;
+    } else {
+      std::cout << "\"Usage:\\n\"\n";
+      std::cout << "\t " << argv[0] << R"( [OPTIONS] [FILE]\n\n")";
+      std::cout << options_text;
+    }
   };
 
-  char GetSpecialChar(const std::string &ch) {
-    if (ch == "\\n")
+  char GetSpecialChar(char ch[]) {
+    if (strncmp(ch, "\\n", 2) == 0)
       return '\n';
-    else if (ch == "\\t")
+    else if (strncmp(ch, "\\t", 2) == 0)
       return '\t';
-    else if (ch == "\\\\")
+    else if (strncmp(ch, "\\\\", 2) == 0)
       return '\\';
-    else if (ch == "\\\"")
+    else if (strncmp(ch, "\\\"", 2) == 0)
       return '\"';
-    else if (ch == "\\'")
+    else if (strncmp(ch, "\\\'", 2) == 0)
       return '\'';
     else {
       ErrorMessage(ErrorCodes::WRONG_DELIMITER, ch);
@@ -55,13 +55,13 @@ class ArgumentParser {
     }
   };
 
-  void GetLines(const std::string &arg, int &i) {
-    std::string temp_lines;
+  void GetLines(char arg[], int &i) {
+    char *temp_lines;
     int lines;
-    if (arg.starts_with("--lines=")) {
-      temp_lines = arg.substr(strlen("--lines="), arg.size() - strlen("--lines="));
-    } else if (arg.starts_with("-l=")) {
-      temp_lines = arg.substr(strlen("-l="), arg.size() - strlen("-l="));
+    if (strncmp(arg, "--lines=", strlen("--lines=")) == 0) {
+      temp_lines = arg + strlen("--lines=");
+    } else if (strncmp(arg, "-l=", strlen("-l=")) == 0) {
+      temp_lines = arg + strlen("-l=");
     } else {
       if (argc > ++i)
         temp_lines = argv[i];
@@ -81,14 +81,12 @@ class ArgumentParser {
     }
   };
 
-  void GetDelimiter(const std::string &arg, int &i) {
-    std::string value;
-    if (arg.starts_with("--delimiter=")) {
-      size_t prefix_len = strlen("--delimiter=");
-      value = arg.substr(prefix_len, arg.size() - prefix_len);
-    } else if (arg.starts_with("-d=")) {
-      size_t prefix_len = strlen("-d=");
-      value = arg.substr(prefix_len, arg.size() - prefix_len);
+  void GetDelimiter(char arg[], int &i) {
+    char *value;
+    if (strncmp(arg, "--delimiter=", strlen("--delimiter=")) == 0) {
+      value = arg + strlen("--delimiter=");
+    } else if (strncmp(arg, "-d=", strlen("-d=")) == 0) {
+      value = arg + strlen("-d=");
     } else {
       if (argc > ++i)
         value = argv[i];
@@ -96,17 +94,17 @@ class ArgumentParser {
         ErrorMessage(ErrorCodes::VALUE_MISSING, arg);
     }
 
-    if (value.starts_with("'")) {
-      if (value.size() == 3) {
+    if (strncmp(value, "'", 1) == 0) {
+      if (strlen(value) == 3) {
         config.delimiter = value[1];
-      } else if (value.size() == 4) {
-        config.delimiter = GetSpecialChar(value.substr(1, value.size() - 2));
+      } else if (strlen(value) == 4) {
+        config.delimiter = GetSpecialChar(value + strlen("\'"));
       } else {
         ErrorMessage(ErrorCodes::WRONG_DELIMITER, value);
       }
-    } else if (value.size() == 1) {
+    } else if (strlen(value) == 1) {
       config.delimiter = value[0];
-    } else if (value.size() == 2) {
+    } else if (strlen(value) == 2) {
       config.delimiter = GetSpecialChar(value);
     } else {
       ErrorMessage(ErrorCodes::WRONG_DELIMITER, value);
@@ -152,29 +150,29 @@ class ArgumentParser {
 
   void ParseArguments() {
     for (int i = 1; i < argc; ++i) {
-      std::string arg = argv[i];
-      if (arg.starts_with("-")) {
-        if (arg == "-h" || arg == "--help") {
+      if (strncmp(argv[i], "-", 1) == 0) {
+        if (strncmp(argv[i], "-h", 2) == 0 || strncmp(argv[i], "--help", 6) == 0) {
           PrintHelpMessage(false);
           exit(0);
-        } else if (arg.contains("-t")) {
+        } else if (strncmp(argv[i], "-t", 2) == 0 || strncmp(argv[i], "--tail", strlen("--tail")) == 0) {
           if (!is_lines_set) {
-            ErrorMessage(ErrorCodes::TAIL_WITHOUT_LINES, arg);
+            ErrorMessage(ErrorCodes::TAIL_WITHOUT_LINES, argv[i]);
           }
           config.tail = true;
-        } else if (arg.contains("-d")) {
-          GetDelimiter(arg, i);
-        } else if (arg.contains("-l")) {
+        } else if (strncmp(argv[i], "-d", 2) == 0 || strncmp(argv[i], "--lines", strlen("--delimiter")) == 0) {
+          GetDelimiter(argv[i], i);
+        } else if (strncmp(argv[i], "-l", 2) == 0 || strncmp(argv[i], "--lines", strlen("--lines")) == 0) {
           is_lines_set = true;
-          GetLines(arg, i);
+          GetLines(argv[i], i);
         } else {
-          ErrorMessage(ErrorCodes::WRONG_ARGUMENT, arg);
+          ErrorMessage(ErrorCodes::WRONG_ARGUMENT, argv[i]);
         }
       } else {
-        config.filename = arg;
+        is_filename_set = true;
+        config.filename = argv[i];
       }
     }
-    if (config.filename.empty()) {
+    if (!is_filename_set) {
       ErrorMessage(ErrorCodes::FILENAME_MISSING, "");
     }
   }
@@ -182,85 +180,97 @@ class ArgumentParser {
 
 class FileReader {
  private:
-  std::string filename;
   Config config;
   std::ifstream file;
+
+  void ReadFileHead() {
+    long long counter = 0;
+    constexpr long long buffer_size = 1024 * 1024;
+    char buffer[buffer_size];
+
+    while (!file.eof()) {
+      file.read(buffer, buffer_size);
+      std::streamsize bytes_read = file.gcount();
+      for (int i = 0; i < bytes_read; ++i) {
+        if (counter >= config.lines) return;
+        if (buffer[i] == config.delimiter) ++counter;
+        std::cout << buffer[i];
+      }
+    }
+  };
+
+  void ReadFileTail() { // прости...
+    long long counter = 0;
+
+    constexpr long long buffer_size = 1024 * 512;
+    char buffer[buffer_size];
+    char tmp_buffer[buffer_size];
+
+    file.seekg(0, std::ios::end);
+    long long file_size = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    long long end_pos = file_size;
+    long long bytes_to_read = std::min(file_size, buffer_size);
+
+    while (bytes_to_read != 0) {
+      file.seekg(file_size - bytes_to_read);
+      file.read(buffer, bytes_to_read);
+
+      for (long long i = bytes_to_read - 1; i > -1; --i) {
+        if (counter >= config.lines) return;
+        if (buffer[i] == config.delimiter) {
+          ++counter;
+          long long current_pos = file_size - bytes_to_read + i + 1;
+          if (file_size > end_pos) {
+            long long tmp_file_size = end_pos - current_pos;
+            long long tmp_bytes_to_read = std::min(tmp_file_size, buffer_size);
+            while (tmp_bytes_to_read != 0) {
+              file.seekg(current_pos);
+              file.read(tmp_buffer, tmp_bytes_to_read);
+              for (long long j = 0; j < tmp_bytes_to_read; ++j) {
+                std::cout << tmp_buffer[j];
+              }
+              end_pos = current_pos - 1;
+              std::cout << config.delimiter;
+              tmp_file_size -= tmp_bytes_to_read;
+              tmp_bytes_to_read = std::min(tmp_file_size, buffer_size);
+            }
+          } else {
+            file.seekg(current_pos);
+            file.read(tmp_buffer, end_pos - current_pos);
+            for (long long j = 0; j < end_pos - current_pos; ++j) {
+              std::cout << tmp_buffer[j];
+            }
+            end_pos = current_pos - 1;
+            std::cout << config.delimiter;
+          }
+        }
+      }
+
+      file_size -= bytes_to_read;
+      bytes_to_read = std::min(file_size, buffer_size);
+    }
+    for (long long i = 0; i < end_pos; ++i) {
+      std::cout << buffer[i];
+    }
+  };
  public:
-  FileReader(std::string filename, Config &config) : filename(std::move(filename)), config(config) {
-    file.open(this->filename);
+  FileReader(char filename[], Config &config) : config(config) {
+    file.open(filename);
   };
 
   bool IsOpen() {
     return file.is_open();
   }
 
-  void ReadFile() { // простите...
-    long long counter = 0;
-    constexpr long long buffer_size = 1024*128;
-    char buffer[buffer_size];
-
+  void ReadFile() {
     if (!config.tail) {
-      while (!file.eof()) {
-        file.read(buffer, buffer_size);
-        int bytes_read = file.gcount();
-        for (int i = 0; i < bytes_read; ++i) {
-          if (counter >= config.lines) return;
-          if (buffer[i] == config.delimiter) ++counter;
-          std::cout << buffer[i];
-        }
-      }
+      ReadFileHead();
     } else {
-      file.seekg(0, std::ios::end);
-      long long file_size = file.tellg();
-      char tmp_buffer[buffer_size];
-      file.seekg(0, std::ios::beg);
-
-      long long end_pos = file_size;
-      long long bytes_to_read = std::min(file_size, buffer_size);
-
-      while (bytes_to_read != 0) {
-        file.seekg(file_size - bytes_to_read);
-        file.read(buffer, bytes_to_read);
-
-        for (long long i = bytes_to_read - 1; i > -1; --i) {
-          if (counter >= config.lines) return;
-          if (buffer[i] == config.delimiter) {
-            ++counter;
-            long long current_pos = file_size - bytes_to_read + i + 1;
-            if (file_size > end_pos) {
-              long long tmp_file_size = end_pos - current_pos;
-              long long tmp_bytes_to_read = std::min(tmp_file_size, buffer_size);
-              while (tmp_bytes_to_read != 0) {
-                file.seekg(current_pos);
-                file.read(tmp_buffer, tmp_bytes_to_read);
-                for (long long j = 0; j < tmp_bytes_to_read; ++j) {
-                  std::cout << tmp_buffer[j];
-                }
-                end_pos = current_pos - 1;
-                std::cout << config.delimiter;
-                tmp_file_size -= tmp_bytes_to_read;
-                tmp_bytes_to_read = std::min(tmp_file_size, buffer_size);
-              }
-            } else {
-              file.seekg(current_pos);
-              file.read(tmp_buffer, end_pos - current_pos);
-              for (long long j = 0; j < end_pos - current_pos; ++j) {
-                std::cout << tmp_buffer[j];
-              }
-              end_pos = current_pos - 1;
-              std::cout << config.delimiter;
-            }
-          }
-        }
-
-        file_size -= bytes_to_read;
-        bytes_to_read = std::min(file_size, buffer_size);
-      }
-      for (long long i = 0; i < end_pos; ++i) {
-        std::cout << buffer[i];
-      }
+      ReadFileTail();
     }
-  }
+  };
 };
 
 int main(int argc, char **argv) {
